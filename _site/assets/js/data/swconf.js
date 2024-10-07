33172ac11e26dd2060b75c8b9e6d1424b01a2937
byunyourim <byourim@google.com>
2024-10-07 14:48:10 +0900

    수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727888654',resources: [
+    cacheName: 'chirpy-1728279953',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
