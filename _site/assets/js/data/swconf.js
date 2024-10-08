b919e89660a49aecf35ce95b7d129c1f70d8d2df
byunyourim <byourim@google.com>
2024-10-08 12:28:05 +0900

    commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728353233',resources: [
+    cacheName: 'chirpy-1728358017',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
